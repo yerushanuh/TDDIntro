8f3f77e5f1e7f0d28967b6285584b4f4331190fb
yerushanuh <yerushanuh@gmail.com>
2015-07-24 20:40:54 -0400

    Passed Account Test 1

---
+++ src/main/java/com/thoughtworks/tddintro/accountbalance/Account.java
@@ -0,0 +1,17 @@
+package com.thoughtworks.tddintro.accountbalance;
+
+/**
+ * Created by Yerusha on 2015-07-24.
+ */
+public class Account {
+    private int balance;
+
+    public Account(int balance) {
+        this.balance = balance;
+    }
+
+    public int deposit(int amount) {
+        this.balance += amount;
+        return this.balance;
+    }
+}
--- src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
@@ -1,11 +1,17 @@
 package com.thoughtworks.tddintro.accountbalance;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class AccountTests {
-//    @Test
-//    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-//
-//    }
-//
+    private final Account account = new Account(100);
+
+    @Test
+    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
+        assertThat(new Account(100).deposit(50), is(150));
+    }
+
 //    @Test
 //    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
 //
