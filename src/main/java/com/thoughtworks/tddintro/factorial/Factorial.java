9ebf76bbcee4a559a2d220fe553fb1dd23e6a76a
yerushanuh <yerushanuh@gmail.com>
2015-07-24 20:11:07 -0400

    Passed Factorial Test 1

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,6 +2,10 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        if (i == 1) {
+            return 1;
+        }
+
         return -1;
     }
 }
